a1fab7acdba72e7a958aff6702c8b56770d3ff4a
Gautam <gomzekh@gmail.com>
2020-12-28 14:27:43 +0530

    Day 6 of Python coding

---
+++ Day 6/Coding-exercise6.1.py
@@ -0,0 +1,18 @@
+# Functions in Python
+# Syntax - def my_function():
+
+# I learned about functions and while loops on Day 6 but it was not directly just simple code so here I am providing link for each exercise.
+
+# The website name is Reeborg'S world
+
+# Coding-exercise5.1:
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
+
+# Coding-exercise5.2:
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle2.json
+
+# Coding-exercise5.3:
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle3.json
+
+# Coding-exercise5.4:
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle4.json
