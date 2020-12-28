91b3f5952aafa6407588ffbed9fd5b9e2baee44a
Gautam <gomzekh@gmail.com>
2020-12-28 16:20:08 +0530

    Day 6 of Python coding

--- Day 6/Coding-exercise6.1.py
+++ Day 6/Coding-exercise6.1.py
@@ -3,16 +3,69 @@
 
 # I learned about functions and while loops on Day 6 but it was not directly just simple code so here I am providing link for each exercise.
 
-# The website name is Reeborg'S world
 
-# Coding-exercise5.1:
+# The website name is Reeborg's world
+
+
+# Coding-exercise5.1: Hurdle 1
 # https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
 
-# Coding-exercise5.2:
+
+# Coding-exercise5.2: Hurdle 2
 # https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle2.json
 
-# Coding-exercise5.3:
+
+# Coding-exercise5.3: Hurdle 3
 # https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle3.json
 
-# Coding-exercise5.4:
+
+# Coding-exercise5.4: Hurdle 4
 # https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle4.json
+
+# Hurdle 4 solution
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+    
+# def jump():
+#     turn_left()
+#     while wall_on_right():
+#         move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     while front_is_clear():
+#         move()
+#     turn_left()
+
+# while not at_goal():
+#     if wall_in_front():
+#         jump()
+#     else:
+#         move()
+
+# Coding-exercise5.5: Maze Problem
+#  https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+
+# Maze Solution
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+    
+# while front_is_clear():
+#     move()
+    
+# turn_left()
+
+# while not at_goal():
+#     if right_is_clear():
+#         turn_right()
+#         move()
+#     elif front_is_clear():
+#         move()
+#     else:
+#         turn_left()
+
+
