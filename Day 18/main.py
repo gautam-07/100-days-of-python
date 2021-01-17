e59a6c7a3069b33217ec17a6ee61b134941c63ba
Gautam <gomzekh@gmail.com>
2021-01-17 20:15:50 +0530

    Day 18 of Python Coding

--- Day 18/main.py
+++ Day 18/main.py
@@ -1,73 +1,60 @@
-# ✥ Title  :- The Graphical User Interface
+# **************************************************
+# ✥ Title  :- Learning Turtle Module
 # ✥ Author :- Gautam Khatter 🧐
 # ✥ Date   :- 17 January 2021
 # **************************************************
-# ✥ Turtle Module
+# ✥ Turtle and Colorgram
 # **************************************************
 
+import turtle as t
+# import colorgram
 
 
-import turtle as T
+# rgb = []
+# colors = colorgram.extract('dots.jpg', 30)
+
+
+# for Color in colors:
+#    r = Color.rgb.r
+#    g = Color.rgb.g
+#    b = Color.rgb.b
+#    color = (r, g, b)
+#    rgb.append(color)
+
+
+
+
+# Drawing Hirst Painting
 import random
 
-timmy = T.Turtle()
-T.colormode(255)
+t.colormode(255)
+tim = t.Turtle()
+tim.speed("fastest")
+tim.penup()
+tim.hideturtle()
+color_list = [(202, 164, 109), (238, 240, 245), (150, 75, 49), (223, 201, 135), (52, 93, 124), (172, 154, 40), (140, 30, 19), (133, 163, 185), (198, 91, 71), (46, 122, 86), (72, 43, 35), (145, 178, 148), (13, 99, 71), (233, 175, 164), (161, 142, 158), (105, 74, 77), (55, 46, 50), (183, 205, 171), (36, 60, 74), (18, 86, 90), (81, 148, 129), (148, 17, 20), (14, 70, 64), (30, 68, 100), (107, 127, 153), (174, 94, 97), (176, 192, 209)]
+tim.setheading(225)
+tim.forward(300)
+tim.setheading(0)
+number_of_dots = 100
 
-def random_color():
-   r = random.randint(0,255)
-   g = random.randint(0,255)
-   b = random.randint(0, 255)
-   color = (r, g, b)
-   return color
+for dot_count in range(1, number_of_dots + 1):
+    tim.dot(20, random.choice(color_list))
+    tim.forward(50)
 
-
-# Task 1: Drawing dashed line
-# for _ in range(15):
-#    timmy.forward(10)
-#    timmy.penup()
-#    timmy.forward(10)
-#    timmy.pendown()
-
-
-
-
-# Task 2: Draw Different shapes
-# timmy.pensize(3)
-# timmy.speed("slowest")
-# def draw(sides):
-#    angle = 360 / sides
-#    for _ in range(sides):
-#       timmy.forward(100)
-#       timmy.right(angle)
-
-# for n in range(3, 11):
-#    timmy.color(random_color())
-#    draw(n)
+    if dot_count % 10 == 0:
+        tim.setheading(90)
+        tim.forward(50)
+        tim.setheading(180)
+        tim.forward(500)
+        tim.setheading(0)
 
 
 
 
-# Task 3: Draw a random walk
-# directions = [0, 90, 180, 270]
-# timmy.pensize(14)
-
-# for _ in range(200):
-#    timmy.color(random_color())
-#    timmy.forward(30)
-#    timmy.setheading(random.choice(directions))
 
 
-
-# Task 4: Draw a Spirograph
-# def draw(gap):
-#    for _ in range(int(360/gap)):
-#       timmy.color(random_color())
-#       timmy.circle(100)
-#       timmy.setheading(timmy.heading() + gap)
-
-# timmy.speed("fastest")
-# draw(5)
-
-
-screen = T.Screen()
+screen=t.Screen()
 screen.exitonclick()
+
+
